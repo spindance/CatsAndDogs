b77a8d9631dddd02e665389c6f50b8e6103d00d8
David van Geest <david.vangeest@gmail.com>
2013-06-28 15:45:22 -0400

    Add an update method to Dogs API, with validations.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -6,6 +6,28 @@ class DogsController < ApplicationController
   def show
   end
 
+  def update
+    @dog.name = params[:name]
+    service = DogService.new
+    begin
+      if service.update_dog(@dog)
+        respond_to do |format|
+          format.json do
+            render
+          end
+        end
+      else
+        respond_to do |format|
+          format.json do
+            render :status => :unprocessable_entity
+          end
+        end
+      end
+    ensure
+      service.close()
+    end
+  end
+
   private
 
   def find_dog
